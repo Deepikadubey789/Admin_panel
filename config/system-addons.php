46b183f3818b5b6236477a9176269966c2246db6
Deepika Dubey <deepikadubey789@gmail.com>
2026-03-17 13:37:54 +0530

    initial commit

---
+++ config/system-addons.php
@@ -0,0 +1,48 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '0',
+    'name' => 'Deepika',
+    'identifier' => 'deepika@gmail.com',
+    'username' => 'EN25CS5010032@medicaps.ac.in',
+    'purchase_key' => 'dfghjk',
+    'software_id' => 'NDAyMjQ3NzI=',
+    'domain' => 'localhost:8000',
+    'software_type' => 'product',
+    'errors' => 
+    array (
+      'domain' => 
+      array (
+        0 => 'The domain does not resolve in DNS.',
+      ),
+    ),
+  ),
+  'provider_app' => 
+  array (
+    'active' => '0',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => '',
+    'errors' => 
+    array (
+    ),
+  ),
+  'serviceman_app' => 
+  array (
+    'active' => '',
+    'name' => '',
+    'identifier' => '',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => '',
+    'errors' => 
+    array (
+    ),
+  ),
+);
